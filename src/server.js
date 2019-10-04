7002a2f40c8f6c367104cb189a50f5ead04625aa
Thauan Braga Almeida <thaubr28@gmail.com>
2019-10-04 08:43:00 -0300

    Criação de models e controller

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const app = express();
-
+mongoose.connect('mongodb+srv://thaubr28:thau012725@rockseat-app-xh9fh.mongodb.net/admin?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 app.use(express.json())
 app.use(routes);
 
